b0be46822f584f93472afbfac8103f67a388bee1
karthik0098 <gandhirajankarthik0098@gmail.com>
2021-11-02 19:55:21 +0530

    part time wages

--- src/com/employee/Wages.java
+++ src/com/employee/Wages.java
@@ -1,26 +1,26 @@
 package com.employee;
 
-public class Wages {
+public class Wages{
 	public static void main(String[] args) {
-		int Ispresent = 1;
-		int parttime =1;
-		double employee = Math.floor(Math.random()*2);
-				System.out.println(employee);	
-		if (employee == Ispresent)
-			{
-		if (parttime == Ispresent) {
-           int wages=160;
-			System.out.println("part time emplloyee is present \n wages" +wages);	
-			}
-//				System.out.println("Employee is present");
-			int	wages = 160;
-//				System.out.println("the employee wages is " +wages);
-			}
-		else 
-		{
-			int wages =0;
-				System.out.println("Employee is absent");
-				System.out.println("the employee wages is " +wages);
-		}
+		double attendance =Math.floor(Math.random()*2);
+		double partTime =Math.floor(Math.random()*2);
+
+		int wages; 
+        	System.out.println(attendance);
+   if(attendance == 1) {
+	   		//System.out.println("Employee is present");
+	   if(partTime == 1) {
+	   		wages=160;
+	   		System.out.println("Employee is present \nwages = " + wages);
+	   		System.out.println("Employee is present & PartTime \nwages = " + wages);
+	   }
+	    else {
+	    	wages=160;
+	   		System.out.println("Employee is present & FullTime \nwages = " + wages);
+	    }
+   }else {
+	         wages=0;
+	   		System.out.println("Employee is absent \nwages = " + wages);
+}
 	}
 }
