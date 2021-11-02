0678ca49ce4d8f9dfee2b2a787ebb51527a18d9b
karthik0098 <gandhirajankarthik0098@gmail.com>
2021-11-02 17:48:20 +0530

    wages calculation

--- src/com/employee/Wages.java
+++ src/com/employee/Wages.java
@@ -2,14 +2,22 @@ package com.employee;
 
 public class Wages {
 	public static void main(String[] args){
-		
 		int Ispresent = 1;
 		double employee = Math.floor(Math.random()*2);
-		System.out.println(employee);	
+				System.out.println(employee);	
 		if (employee == Ispresent)
+			{
 				System.out.println("Employee is present");
-			else 
+			int	wages = 160;
+				System.out.println("the employee wages is " +wages);
+			}
+		else 
+		{
+			int wages =0;
 				System.out.println("Employee is absent");
-	
+				System.out.println("the employee wages is " +wages);
+		}
 	}
 }
+
+		
