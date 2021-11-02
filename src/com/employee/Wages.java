4bf64b555c803e1d481f42893763ac24257b6a4e
karthik0098 <gandhirajankarthik0098@gmail.com>
2021-11-02 17:22:33 +0530

    employee attendance

---
+++ src/com/employee/Wages.java
@@ -0,0 +1,15 @@
+package com.employee;
+
+public class Wages {
+	public static void main(String[] args){
+		
+		int Ispresent = 1;
+		double employee = Math.floor(Math.random()*2);
+		System.out.println(employee);	
+		if (employee == Ispresent)
+				System.out.println("Employee is present");
+			else 
+				System.out.println("Employee is absent");
+	
+	}
+}
